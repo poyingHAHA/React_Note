3420a9cf017e04c35b780bc55cd7b660d90379d6
poyingHAHA <poying.code@gmail.com>
2026-02-09 21:20:24 +0800

    3. use Link instead of a tag to avoid browser fetch whole js bundle from server

--- react_router/src/pages/Home.jsx
+++ react_router/src/pages/Home.jsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom"
 
 export default function Home() {
     return <>
         <h1>Home Page</h1>
+        <p>Go to <Link to="/products">Products</Link></p>
     </>
 }
